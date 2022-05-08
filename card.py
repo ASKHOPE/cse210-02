377ab93a8d7a0d2a77200d313a8919910d6431e6
karrassphiri <97294565+karrassphiri@users.noreply.github.com>
2022-05-08 17:21:56 +0200

    this is a new commit of card.py file

--- card.py
+++ card.py
@@ -3,6 +3,8 @@ import random
 # one number from the numbers list. Store the chosen
 # number in a variable named number.
 # Author:Karrass
+
+
 class Card:
     """
     Responsibility of the Card class is to get a random number for displayed card 
@@ -12,14 +14,13 @@ class Card:
       value(int): it will store the random number from 1 to 13 
 
     """
+# Author:Karrass
 
     def __init__(self):
         self.value = 0
 
-    
     def get_random_card(self):
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-        
         number = random.choice(numbers)
         self.value = number
